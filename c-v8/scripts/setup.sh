e6ee92dce4a94e861ab85b88104e7b7523c75dad
r4xjs <r4xjs@lowsec.net>
2020-05-26 22:56:50 +0200

    add c-v8 container

---
+++ c-v8/scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <commit>"
+    exit 1
+fi
+
+commit="$1"
+
+if [ ! -d /share/v8 ]; then
+   mkdir /share/v8
+   pushd /share/v8
+     fetch v8
+   popd
+fi
+
+pushd /share/v8
+  git checkout "$commit"
+  gclient sync
+  ./build/install-build-deps.sh
+popd
